cebb071baaa7805976a4a8a8860bc95c2fea2349
Django_Practice <nkniteshstar@gmail.com>
2023-05-24 00:14:20 +0530

    Updating Day 5: ConditionalCommand File.

--- Day 5/ConditionalCommand.py
+++ Day 5/ConditionalCommand.py
@@ -10,4 +10,36 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(service=serv_obj, options=options)
-driver.get("https://demo.nopcommerce.com/register")
+driver.get("https://demo.nopcommerce.com/")
+
+driver.maximize_window()
+
+# is_displayed() and is_enabled  (WEB ELEMENT) -> used for validation purpose
+
+search_box = driver.find_element(By.XPATH,"//input[@id='small-searchterms']")
+
+print("Display Status:", search_box.is_displayed())     # True
+print("Enable Status:", search_box.is_enabled())        # True
+
+# is_selected()  is for radioButton and checkBox
+
+rd_male = driver.find_element(By.XPATH,"//input[@id='gender-male']")
+rd_female = driver.find_element(By.XPATH,"//input[@id='gender-female']")
+
+print("Default radio button status.")
+print("Male Status: ", rd_male.is_selected())        # False
+print("Female Status: ", rd_female.is_selected())      # False
+
+rd_male.click()
+
+print("After selecting a radio button.")
+print("Male Status: ", rd_male.is_selected())        # True
+print("Female Status: ", rd_female.is_selected())      # False
+
+rd_female.click()
+
+print("After selecting a radio button.")
+print("Male Status: ", rd_male.is_selected())        # False
+print("Female Status: ", rd_female.is_selected())      # True
+
+driver.quit()
